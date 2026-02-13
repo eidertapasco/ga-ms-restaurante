ceaf72dd2c914b33800c1a1385dd5c2181ecfcbe
Eider Tapasco <eidertapasco.dev@gmail.com>
2026-02-13 02:36:46 -0500

    Initial commit: Se creo la estructura base del modulo restaurante

---
+++ src/main/java/co/edu/sena/ga_ms_restaurante/GaMsRestauranteApplication.java
@@ -0,0 +1,13 @@
+package co.edu.sena.ga_ms_restaurante;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GaMsRestauranteApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(GaMsRestauranteApplication.class, args);
+	}
+
+}
---
+++ src/test/java/co/edu/sena/ga_ms_restaurante/GaMsRestauranteApplicationTests.java
@@ -0,0 +1,13 @@
+package co.edu.sena.ga_ms_restaurante;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GaMsRestauranteApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
