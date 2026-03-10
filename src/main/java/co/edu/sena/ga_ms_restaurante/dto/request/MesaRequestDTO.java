99ae27d491b88e4ca292206fe04aa4e578324ec1
Eider Tapasco <eidertapasco.dev@gmail.com>
2026-03-09 20:58:17 -0500

    feat: Cree la logica de mesas en la clase MesaService, tuve que hacer ediciones minimas a algunos archivos porque generaban conflictos de tipo de dato o de nombre de variables.

--- src/main/java/co/edu/sena/ga_ms_restaurante/dto/request/MesaRequestDTO.java
+++ src/main/java/co/edu/sena/ga_ms_restaurante/dto/request/MesaRequestDTO.java
@@ -3,6 +3,7 @@ package co.edu.sena.ga_ms_restaurante.dto.request;
 import jakarta.validation.constraints.Min;
 import jakarta.validation.constraints.NotBlank;
 import jakarta.validation.constraints.NotNull;
+import jakarta.validation.constraints.Pattern;
 import lombok.AllArgsConstructor;
 import lombok.Builder;
 import lombok.Data;
@@ -13,11 +14,14 @@ import lombok.NoArgsConstructor;
 @NoArgsConstructor
 @AllArgsConstructor
 public class MesaRequestDTO {
-    @NotNull(message = "El número de mesa es obligatorio")
-    @Min(value = 1, message = "El número de mesa debe ser mayor a 0")
-    private Integer numeroMesa;
 
-    @NotBlank(message = "La capacidad es obligatoria")
-    private String capacidad;
-    // Se elimina 'estado', lo maneja el Service internamente.
+    @NotBlank(message = "El identificador de la mesa es obligatorio")
+    @Pattern(regexp = "^[a-zA-Z0-9-]+$", message = "El número de mesa solo acepta letras, números y guiones (ej. M-1)")
+    private String numeroMesa;
+
+    @NotNull(message = "La capacidad es obligatoria")
+    @Min(value = 1, message = "La capacidad debe ser mayor a 0")
+    private Integer capacidad;
+
+    // Se elimina 'estado', lo maneja el Service internamente, la mesa nace 'DISPONIBLE'.
 }
